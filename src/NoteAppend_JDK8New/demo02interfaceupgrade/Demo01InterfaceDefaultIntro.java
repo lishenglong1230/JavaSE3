b7fa0596475a7f9794ffef055468d53b1211dcd1
“lishenglong1230” <1215288388@qq.com>
2022-08-26 17:40:33 +0800

    接口增强

---
+++ src/NoteAppend_JDK8New/demo02interfaceupgrade/Demo01InterfaceDefaultIntro.java
@@ -0,0 +1,21 @@
+package NoteAppend_JDK8New.demo02interfaceupgrade;
+
+
+interface A {
+    public abstract void test01();
+    // public abstract void test02();
+}
+
+class B implements A {
+    @Override
+    public void test01() {
+        System.out.println("B test01");
+    }
+}
+
+class C implements A {
+    @Override
+    public void test01() {
+        System.out.println("C test01");
+    }
+}
